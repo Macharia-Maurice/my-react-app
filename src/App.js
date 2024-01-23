db1fbc7bbe6b079fd78f1faa25c2611036881fd5
maurice <maurice.maina@students.jkuat.ac.ke>
2024-01-23 18:45:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import ReactPlayer from 'react-player';
 import './App.css';
 
+
 function App() {
+  const vidurl="https://youtu.be/zaIsVnmwdqg"
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <ReactPlayer
+        url={vidurl}
+        volume={0.5}
+        playing={false}
+        controls
+        />
       </header>
     </div>
   );
