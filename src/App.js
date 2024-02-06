7bcfceb5f1bc60c95d4ea631f20358bcef5fad7d
maurice <maurice.maina@students.jkuat.ac.ke>
2024-02-06 20:47:33 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import ReactPlayer from 'react-player';
 import './App.css';
-
-
 function App() {
-  const vidurl="https://youtu.be/zaIsVnmwdqg"
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <ReactPlayer
-        url={vidurl}
-        volume={0.5}
-        playing={false}
-        controls
-        />
+
       </header>
     </div>
   );
